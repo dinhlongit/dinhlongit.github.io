fbe8539742b70527bceb6b91710dbd18493abd10
Nguyen Dinh Long <zingnewtop@gmail.com>
2019-07-06 18:41:53 +0700

    alolong

---
+++ thongtindangky.php
@@ -0,0 +1,96 @@
+<?php 
+
+$hoten = $_GET['fullname'];
+
+echo $hoten;
+
+
+?>
+
+<!doctype html>
+<html lang="vi" xmlns:fb="http://ogp.me/ns/fb#"> 
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Hue Marathon 2019">
+  <title>HUE MARATHON</title>
+  <!-- <link rel="icon" href="img/hue_fav_icon.png" type="image/gif" sizes="16x16"> -->
+  <link rel="stylesheet" href="css/bootstrap-material-design.min.css">
+  <link rel="stylesheet" type="text/css" href="css/fontawesome-v5-all.css">
+  <link rel="stylesheet" type="text/css" href="css/hover.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" type="text/css" href="css/font-awesome-animation.css">
+  <link rel="stylesheet" type="text/css" href="css/jquery.fancybox.min.css">
+  <link rel="stylesheet" type="text/css" href="css/slick.css">
+  <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+  <link rel="stylesheet" type="text/css" href="css/direction-reveal.css">
+  <link rel="stylesheet" type="text/css" href="css/style_2019.css">
+</head>
+<body>
+
+  <div class="container-fluid">
+    <div class="form-marathon">
+      <div class="container">
+        <div class="text-center title-form" id="tabRegister">CHÚC MỪNG BẠN ĐÃ ĐĂNG KÝ THÀNH CÔNG !</div>
+        <div class="row" style="border-radius: 5px;">     
+    </div>
+  </div>
+      </div>
+	      </div>
+		<div class="container">  
+		<div class="thongtin">
+	  <h2 align="center">THÔNG TIN ĐĂNG KÝ CỦA BẠN NHƯ SAU: </h2>  
+	  <br>
+	  <ul>
+	
+	   <li> <strong>Cự Ly Đăng Ký:</strong>  </li>
+  <li> <strong>Họ Tên:</strong> </li>
+  <li><strong> Quốc Tịch:</strong></li>
+  <li><strong>Ngày Sinh:</strong></li>
+  <li><strong>Size Áo:</strong> </li>
+  <li><strong>Tổng Mức Phí:</strong>  </li>
+	  </ul>
+	  <br>
+	   <h3 align="center">THANH TOÁN PHÍ ĐĂNG KÝ HUE HALF MARATHON 2019</h3>  
+	   <p align="center"><strong>Vui lòng chuyển khoản đúng số tiền: trong vòng 72h đến 1 trong các tài khoản ngân hàng sau:</strong></p>
+	   <br>
+<p>1. Ngân hàng TMCP Ngoại thương Việt Nam Vietcombank- Chi nhánh Huế</p>
+<p><b><strong>STK:</b></strong> 0161001727754, Chủ tài khoản : Công ty TNHH MTV Huế Năng Động</p>
+2. Ngân hàng TMCP Việt Nam Thịnh Vượng VPBank - Chi nhánh Huế</p>
+<p><b><strong>STK:</b></strong> 154452378, Chủ tài khoản : Công ty TNHH MTV Huế Năng Động</p>
+  
+
+ <br>
+
+<p>Nếu bạn là học sinh, sinh viên : </p>
+<p><b><strong>Bạn đăng ký được giảm giá 50%</b></strong> , ngày 28/9/2019 khi nhận kit bạn vui lòng mang theo giấy tờ chứng mình là học sinh/sinh viên</p>
+<p><b><strong>* Sau Khi Nhận Được Phí Đăng Ký, BTC Sẽ Gửi Email Xác Nhận Đăng Ký Thành Công Trong Vòng 24h.</b></strong> </p>
+<p>Thông tin chi tiết sẽ được cập nhật thường xuyên tại website sự kiện.</p>
+
+ <hr>
+ 
+ <p align="center">Hue Marathon được tổ chức tại thành phố Huế mộng mơ, thành phố xanh, sạch với lối sống lành mạnh bậc nhất Việt Nam, thành phố của di sản và văn hóa chào đón bạn.</p>
+<h4 align="center">HUE MARATHON TEAM</h4>
+
+ </div>
+
+  </div>
+ 
+</script>
+
+
+<a id="back-to-top" href="#" class="back-to-top hvr-float" role="button" title="Click để lên đầu trang" style="display: none;"><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="js/jquery-3.2.1.min.js"></script> 
+<script type="text/javascript" src="js/slick.min.js"></script>
+<script type="text/javascript" src="js/direction-aware.js"></script>
+<script type="text/javascript" src="js/jquery.fancybox.min.js"></script>
+<script type="text/javascript" src="js/jquery.easeScroll.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap-material-design.js"></script>
+<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+<script type="text/javascript" src="js/custom-2019.js"></script>	
+
+</body>
+</html>
